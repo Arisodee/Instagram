05d114b271d9a80692f30a77fdc6ac3a4339b534
Arisodee <arisodee@gmail.com>
2021-01-17 16:03:15 +0300

    fix errors and test if bootstrap and custom css are working

--- instashots/settings.py
+++ instashots/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instagram',
-    'bootsrtap3',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -120,5 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
